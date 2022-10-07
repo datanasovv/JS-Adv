eb9020682896d8c0cecdeef5d9bbc8a9b2fffe03
Dimitar Atanasov <d.atanasovv96@gmail.com>
2022-10-07 20:26:29 +0300

    tasks solved

--- AdvancedFuntionsProblems/06. Next Article/script.js
+++ AdvancedFuntionsProblems/06. Next Article/script.js
@@ -1,3 +1,13 @@
-function getArticleGenerator(articles) {
-    // TODO
+function getArticleGenerator(input) {
+  let articles = input;
+
+  return () => {
+    if (articles.length) {
+      let container = document.getElementById("content");
+      let article = document.createElement("article");
+      let currentText = articles.shift();
+      article.innerText = currentText;
+      container.appendChild(article);
+    }
+  };
 }
--- AdvancedFuntionsProblems/07. Central Cinema/solution.js
+++ AdvancedFuntionsProblems/07. Central Cinema/solution.js
@@ -1,3 +1,15 @@
 function solve() {
-    // TODO
+  let onScreenBtn = document.getElementById("onScreenBtn");
+  onScreenBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    let movieName = document.getElementById("inputName");
+    let movieHall = document.getElementById("inputHall");
+    let moviePrice = document.getElementById("inputPrice");
+    let moviesUl = document.getElementById("moviesUl");
+
+    let movieInfo = `${movieName.value} | Hall number: ${movieHall.value} | Ticket price: ${moviePrice.value}$`;
+    let newMovie = document.createElement("li");
+    newMovie.textContent = movieInfo;
+    moviesUl.appendChild(newMovie);
+  });
 }
